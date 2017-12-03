d097193f58e10fc87ddc3435fd4a6c21edfc6d69
https://github.com/petemi09 <petemi09@luter.edu>
2017-12-02 21:55:01 -0600

    exercise 11 complete

--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -1,10 +1,12 @@
 package exercise11;
 
+import java.util.ArrayList;
 import java.util.Collection;
+
 
 /**
  * Exercise 11 logic
- * @author yasiro01
+ * @author garipa01 / Patrick Garin
  */
 public class Ex11Logic {
   
@@ -14,6 +16,10 @@ public class Ex11Logic {
    * @param item
    * @return length of the parameter's string representation
    */
+    
+    public <T> int strLen(T item){
+        return item.toString().length();
+    }
   // TODO: Implement method strLen
   
   /**
@@ -25,15 +31,29 @@ public class Ex11Logic {
    * @return the greater of three parameters
    */
   // TODO: Implement method greater
+    public <T extends Comparable> T greater(T thing_1, T thing_2, T thing_3){
+        if(thing_1.compareTo(thing_2) >= 0 && thing_1.compareTo(thing_3) >= 0){
+            return thing_1;
+            } else if(thing_2.compareTo(thing_3) >= 0) {
+                return thing_2;
+                } else {
+                    return thing_3;
+        }
+    }
   
   /**
    * Task 3
-   * @param <T>
-   * @param list of objects
+   // @param <T>
+   // @param list of objects
    * print a list of items
    */
   // TODO: Implement method printList
-  
+  public <T> void printList(T ... list){
+      // '...' provides a empty list space to pass through parameter
+      for(T t: list){
+          System.out.print(t.toString() + " ");
+      }
+  }
   /**
    * Task 4
    * @param <T>
@@ -41,7 +61,23 @@ public class Ex11Logic {
    * @return the smallest item in the collection
    */
   // TODO: Implement method findMinItem
+  public <T extends Comparable> T findMinItem(Collection<T> list){
+      try {
+      T small = list.iterator().next();
+      for(T t: list){
+          if(t.compareTo(small) < 0){
+              small = t;
+          }
+      }
+      return small;
+      } catch(IndexOutOfBoundsException e){
+          System.out.println("Error");
+          return null;
+      }
+  
+    }
 }
+
 
   /**
    * Task 5
@@ -51,3 +87,37 @@ public class Ex11Logic {
    * Implement the constructor, getters, setters, and toString
    */
   // TODO: Implement class Foo
+
+
+class Foo<T,U> {
+    protected T fuzz;
+    protected U buzz;
+
+    public Foo(T fuzz, U buzz) {
+        this.fuzz = fuzz;
+        this.buzz = buzz;
+    }
+
+    public T getFuzz() {
+        return fuzz;
+    }
+
+    public void setFuzz(T fuzz) {
+        this.fuzz = fuzz;
+    }
+
+    public U getBuzz() {
+        return buzz;
+    }
+
+    public void setBuzz(U buzz) {
+        this.buzz = buzz;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" + "fuzz=" + fuzz + ", buzz=" + buzz + '}';
+    }
+    
+}
+
